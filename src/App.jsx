5c80648c7b54ea183af0a024d544f1d698177ee0
Mateo Leiva <mateoleiva50@gmail.com>
2024-09-16 21:05:54 -0300

    imagenes de personas

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,44 @@ import { Navbar } from "./componentes/navbar";
 import { Peluqueros } from "./componentes/peluqueros";
 import { Servicios } from "./componentes/servicios";
 import { Footer } from "./componentes/footer";
-import juanImage from "../public/images/Juan.jpg";
-import pedroImage from "../public/images/Pedro.jpg";
+import corteImage1 from "../public/images/corteImage1.jpg";
+import corteImage2 from "../public/images/corteImage2.jpg";
 import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const images = [pedroImage, juanImage];
+  const corteImages = [corteImage1, corteImage2];
   const servicesRef = useRef(null);
   const barbersRef = useRef(null);
 
   const [darkMode, setDarkMode] = useState(() => {
-    const savedTheme = localStorage.getItem("theme")
+    const savedTheme = localStorage.getItem("theme");
     if (savedTheme) {
-      return savedTheme === "dark"
-    }
-    else {
-      const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      return savedTheme === "dark";
+    } else {
+      const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
       return prefersDark;
     }
-  })
+  });
 
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add("dark");
-      document.documentElement.classList.add("scrollbar-dark")
-      document.documentElement.classList.remove("scrollbar-light")
+      document.documentElement.classList.add("scrollbar-dark");
+      document.documentElement.classList.remove("scrollbar-light");
       localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("scrollbar-light")
-      document.documentElement.classList.remove("scrollbar-dark")
+      document.documentElement.classList.add("scrollbar-light");
+      document.documentElement.classList.remove("scrollbar-dark");
       localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
 
   const toggleDarkMode = () => {
-    setDarkMode(prevMode => !prevMode);
+    setDarkMode((prevMode) => !prevMode);
   };
-
 
   const scrollToSection = (ref) => {
     ref.current.scrollIntoView({ behavior: "smooth" });
@@ -62,18 +62,15 @@ function App() {
         <Peluqueros nombre="Juan" />
         <Peluqueros nombre="Adolfo" />
       </div>
-      <h2 ref={servicesRef} className="text-yellow-500 text-2xl p-4"><strong>Servicios</strong></h2>
-        <Servicios
-          servicio="Corte de Pelo"
-          descripcion="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellat exercitationem ipsa fuga labore molestias error debitis tenetur sapiente quasi vitae quidem fugiat ducimus, similique rem mollitia commodi voluptas maxime."
-          imagenes={images}
-        />
-          <Servicios
-          servicio="Corte de Pelo"
-          descripcion="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellat exercitationem ipsa fuga labore molestias error debitis tenetur sapiente quasi vitae quidem fugiat ducimus, similique rem mollitia commodi voluptas maxime."
-          imagenes={images}
-        />
-      <Footer darkMode={darkMode}/>
+      <h2 ref={servicesRef} className="text-yellow-500 text-2xl p-4">
+        <strong>Servicios</strong>
+      </h2>
+      <Servicios
+        servicio="Corte de Pelo"
+        descripcion="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellat exercitationem ipsa fuga labore molestias error debitis tenetur sapiente quasi vitae quidem fugiat ducimus, similique rem mollitia commodi voluptas maxime."
+        imagenes={corteImages}
+      />
+      <Footer darkMode={darkMode} />
     </>
   );
 }
--- src/componentes/peluqueros.jsx
+++ src/componentes/peluqueros.jsx
@@ -2,7 +2,11 @@ export function Peluqueros({ nombre }) {
   return (
     <div className="text-center max-w-xs w-full p-2">
       <strong className="block">{nombre}</strong>
-      <img className="h-28 md:h-64 w-full object-cover grayscale hover:grayscale-0 transition-all duration-500" src={`./images/${nombre}.jpg`} alt={`Foto de ${nombre}`} />
+      <img
+        className="h-40 md:h-96 w-full object-contain grayscale hover:grayscale-0 transition-all duration-500"
+        src={`./images/${nombre}.jpg`}
+        alt={`Foto de ${nombre}`}
+      />
     </div>
   );
 }
